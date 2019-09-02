cb5b03ffc2b0bf05b3a2b5c368eb592cd8a821d8
mengmanzbhok <1361006944@qq.com>
2019-09-02 21:25:35 +0800

    Update dockerAndComposeInstallScript.sh

--- dockerAndComposeInstallScript.sh
+++ dockerAndComposeInstallScript.sh
@@ -2,7 +2,7 @@
 curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker root
 
-curl -L https://get.daocloud.io/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo mv ~/docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo service docker restart
