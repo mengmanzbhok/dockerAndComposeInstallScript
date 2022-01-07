110df179558db0b8153e96de8d361f98267af993
yangmi <1361006944@qq.com>
2022-01-07 11:37:52 +0800

    Update dockerAndComposeInstallScript.sh

--- dockerAndComposeInstallScript.sh
+++ dockerAndComposeInstallScript.sh
@@ -2,7 +2,7 @@
 curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker root
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo mv ~/docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo service docker restart
