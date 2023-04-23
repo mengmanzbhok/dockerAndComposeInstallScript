2d507128e9c080f869bf19325a1254bed2e029ad
yangmi <1361006944@qq.com>
2023-04-23 10:08:30 +0800

    Update dockerAndComposeInstallScript.sh

--- dockerAndComposeInstallScript.sh
+++ dockerAndComposeInstallScript.sh
@@ -7,7 +7,7 @@ apt-get install tmux
 curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker root
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo mv ~/docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo service docker restart
